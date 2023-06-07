109877ff622cfeee8e43094a223cfda098206e5d
G284452022 <105624111+G284452022@users.noreply.github.com>
2023-06-07 16:02:03 +0900

    kazu

--- kazuate.js
+++ kazuate.js
@@ -19,8 +19,26 @@ function hantei() {
   let yoso = 4;
   
   // 課題3-1: 正解判定する
-if(yoso==kotae){
-    
+if(yoso==kotae && kaisu<2){
+    let kaisu=kaisu+1;
+    console.log(kaisu+'回目の予想: '+yoso);
+    console.log('正解です．おめでとう!');
+}else if(yoso>kotae && kaisu<2){
+  let kaisu=kaisu+1;
+  console.log(kaisu+'回目の予想: '+yoso);
+  console.log('まちがい、答えはもっと小さいですよ');
+}else if(yoso<kotae && kaisu<2){
+  let kaisu=kaisu+1;
+  console.log(kaisu+'回目の予想: '+yoso);
+  console.log('まちがい、答えはもっと大きいですよ');
+}else if(kaisu==2){
+  let kaisu=kaisu+1;
+  console.log(kaisu+'回目の予想: '+yoso);
+  console.log('まちがい．残念でした答えは '+kotae+' です．');
+}else{
+  let kaisu=kaisu+1;
+  console.log(kaisu+'回目の予想: '+yoso);
+  console.log('答えは '+kotae+' でした．すでにゲームは終わっています');
 }
   // kotae と yoso が一致するかどうか調べて結果を出力
 

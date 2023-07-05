27558fcb77e7aff70e045da46d8ff5957393b078
G284452022 <105624111+G284452022@users.noreply.github.com>
2023-07-05 10:11:05 +0900

    kazu

--- kazuate.js
+++ kazuate.js
@@ -8,63 +8,43 @@ let kaisu = 0;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
-hantei();
-
+let a = document.querySelector('button#kaitou');
+a.addEventListener('click',hantei);
+let b = document.querySelector('p#result');
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+  let c = document.querySelector('input[name="seisu"]');
+  let yoso = Number(c.value);
 
-  kaisu=kaisu+1;
-    
+ 
+  let d = document.querySelector('span#kaisu');
+  d.textContent = kaisu + 1;
+  let e = document.querySelector('span#answer');
+  e.textContent = yoso ;
+  if(kaisu>=3){
+    b.textContent = '答え' + kotae + 'でした。すでにゲームは終わっています';
+  }else{
+    if(kaisu==2){
+      if(yoso==kotae){
+        b.textContent = ("正解おめでとう！");
+      }else{
+        b.textContent =("まちがい,残念でした答えは " + kotae + "です.");
+      }
+    }else{
+      if(kotae==yoso){
+        b.textContent = ("正解おめでとう!");
+      }else if(yoso < kotae){
+        b.textContent = ("まちがい.答えはもっと大きいです.");
+      }else{
+        b.textContent = ("まちがい.答えはもっと小さいです.");
+
+      }
+    }
+  }
+  kaisu = kaisu + 1;
   // 課題3-1: 正解判定する
-if(yoso==kotae && kaisu<3){
-  kaisu = document.querySelector('span#kaisu');
-  yoso = document.querySelector('span#answer');
-  p = document.createElement('p');
-  p.textContent = '回目の予想: ';
-  p.textContent = '正解です．おめでとう!';
-  
-   // console.log(kaisu+'回目の予想: '+yoso);
-    //console.log('正解です．おめでとう!');
-
-}else if(yoso>kotae && kaisu<3){
-  kaisu = document.querySelector('span#kaisu');
-  yoso = document.querySelector('span#answer');
-  p = document.createElement('p');
-  p.textContent = '回目の予想: ';
-  p.textContent = 'まちがい、答えはもっと小さいですよ';
-  //console.log(kaisu+'回目の予想: '+yoso);
-  //console.log('まちがい、答えはもっと小さいですよ');
-}else if(yoso<kotae && kaisu<3){
-  kaisu = document.querySelector('span#kaisu');
-  yoso = document.querySelector('span#answer');
-  p = document.createElement('p');
-  p.textContent = '回目の予想: ';
-  p.textContent = 'まちがい、答えはもっと大きいですよ';
-  //console.log(kaisu+'回目の予想: '+yoso);
-  //console.log('まちがい、答えはもっと大きいですよ');
-}else if(kaisu==3){
-  kaisu = document.querySelector('span#kaisu');
-  yoso = document.querySelector('span#answer');
-  p = document.createElement('p');
-  p.textContent = '回目の予想: ';
-  p.textContent = 'まちがい．残念でした答えは '+kotae+' です．';
-  //console.log(kaisu+'回目の予想: '+yoso);
-  //console.log('まちがい．残念でした答えは '+kotae+' です．');
-}else{
-  kaisu = document.querySelector('span#kaisu');
-  yoso = document.querySelector('span#answer');
-  p = document.createElement('p');
-  p.textContent = '回目の予想: ';
-  p.textContent = '答えは '+kotae+' でした．すでにゲームは終わっています';
-  //console.log(kaisu+'回目の予想: '+yoso);
-  //console.log('答えは '+kotae+' でした．すでにゲームは終わっています');
-}
   // kotae と yoso が一致するかどうか調べて結果を出力
-
   // 課題3-1における出力先はコンソール
-}
+  
+  }
